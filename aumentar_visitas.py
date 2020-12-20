bf46823f93081098a715671f0c90dcfed25d6052
franzpc <franzpc@gmail.com>
2020-12-20 12:06:27 -0500

    Update aumentar_visitas.py

--- aumentar_visitas.py
+++ aumentar_visitas.py
@@ -1,21 +1,21 @@
-# Aumentar las visitas de YouTube con Python
+# Crear un bot para aumentar las visitas de YouTube con Python
 
 #librerías (se requiere instalar selenium / descargar webdriver)
 import time;
 from selenium import webdriver;
 
 #Tiempo para refrescas la página
-Timer = 4
+Timer = 5
 
 #Enlace (Blog, YouTube)
-link = 'https://acolita.com'
+enlace = 'https://www.youtube.com/watch?v=Auzs95InJzo'
 
-#number of views
-views = 12520
+#Número visitas
+views = 1000
 
 #driver
 driver = webdriver.Chrome("C:/gisbook/chromedriver.exe")
-driver.get(link)
+driver.get(enlace)
 
 for i in range(views):
     time.sleep(Timer)
